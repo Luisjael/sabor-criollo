2ffcde375892dbf5bf27bd8d2a42bbd765dacd98
Luisjael <friasjaelsantana@gmail.com>
2026-09-03 19:06:33 -0400

    Corregir sintaxis de app.js

--- app.js
+++ app.js
@@ -165,7 +165,9 @@ function renderModel(media, dish) {
   mv.setAttribute("ar-modes", "webxr scene-viewer quick-look");
   mv.setAttribute("ar-placement", "floor");
   mv.setAttribute("ar-scale", "auto");
-  mv.setAttribute("scale", "0.11 0.11 0.11");`n  mv.setAttribute("exposure", "1.5");`n  mv.setAttribute("shadow-intensity", "1");
+  mv.setAttribute("scale", "0.11 0.11 0.11");
+  mv.setAttribute("exposure", "1.5");
+  mv.setAttribute("shadow-intensity", "1");
   media.appendChild(mv);
 }
 
